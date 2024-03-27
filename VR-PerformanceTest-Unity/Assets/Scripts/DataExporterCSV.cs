263615ef1ccf30d5f6609bfcb0b40f68f0bef4f4
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2024-03-27 11:37:00 +0100

    feat: (WIP) created a class that will export data to a csv file

---
+++ VR-PerformanceTest-Unity/Assets/Scripts/DataExporterCSV.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataExporterCSV : MonoBehaviour
+{
+    public string filename = "UnityPerformanceReport";
+}
