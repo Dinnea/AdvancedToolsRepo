345739af81e700e6c16b81368edde8acd76aeb3b
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2024-05-15 11:33:44 +0200

    feat: CSV exporter update
    Exports number of meshes and total nr of triangles
    TODO: frame rate, CPU, GPU, RAM usage

--- VR-PerformanceTest-Unity/Assets/Scripts/DataExporterCSV.cs
+++ VR-PerformanceTest-Unity/Assets/Scripts/DataExporterCSV.cs
@@ -6,4 +6,42 @@ using System.IO;
 public class DataExporterCSV : MonoBehaviour
 {
     public string filename = "UnityPerformanceReport";
+    private static string _filepath;
+
+    private void Start()
+    {
+        _filepath = GetFilePath();
+        //Debug.Log(_filepath);
+        generateLabels();
+    }
+
+    public static void WriteCSV_TEST(int iterations)
+    {   
+        TextWriter writer = new StreamWriter(_filepath, false);
+        for (int i = 0; i < iterations; i++)
+        {
+            writer.WriteLine("ABC");
+        }
+        writer.Close();
+    }
+
+    public static void ExportResults(int objNumber)
+    {
+        TextWriter writer = new StreamWriter(_filepath, true);
+        writer.WriteLine(objNumber.ToString()+","+(960*objNumber).ToString());
+        writer.Close();
+        Debug.Log("Line written.");
+    }
+
+    private void generateLabels()
+    {
+        TextWriter writer = new StreamWriter(_filepath, false);
+        writer.WriteLine("Objects, Triangles, Av. Frame Rate, CPU Usage, GPU Usage, RAM usage");
+        writer.Close();
+    }
+    private string GetFilePath()
+    {
+        return Application.persistentDataPath + "/" + filename + ".csv";
+        //return Application.dataPath + "/" + filename + ".csv";
+    }
 }
